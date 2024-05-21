a66011881e2f4225ef5334afa2c96e4686d43ad3
dev-junehee <dev.junehee@gmail.com>
2024-05-21 18:55:19 +0900

    Design: AutoLayout 사용한 메인 화면 UI 구성

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    
+    @IBOutlet var mainImage: UIImageView!
+    
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var heightTextField: UITextField!
+    
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var weightTextField: UITextField!
+    
+    @IBOutlet var randomButton: UIButton!
+    @IBOutlet var resultButton: UIButton!
+    
+    // 문자열 상수 데이터
+    let messages: [String: String] = [
+        "title": "BMI Calculator",
+        "subTitle": "당신의 BMI 지수를\n알려드릴게요.",
+        "height": "키가 어떻게 되시나요?",
+        "weight": "몸무게는 어떻게 되시나요?",
+        "random": "랜덤으로 BMI 계산하기",
+        "result": "결과 확인하기"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureInitialUI()
+    }
+    
+    // 메인화면 초기 UI 구성
+    // ( 추후 별도 함수로 분리하기!!!!!! )
+    func configureInitialUI() {
+        titleLabel.text = messages["title"]
+        titleLabel.backgroundColor = .clear
+        titleLabel.font = .systemFont(ofSize: 26, weight: .bold)
+        
+        subTitleLabel.text = messages["subTitle"]
+        subTitleLabel.numberOfLines = 0
+        subTitleLabel.backgroundColor = .clear
+        subTitleLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        
+        mainImage.image = UIImage(named: "image")
+        mainImage.backgroundColor = .clear
+        mainImage.contentMode = .scaleAspectFit
+        
+        heightLabel.text = messages["height"]
+        heightLabel.textAlignment = .left
+        heightLabel.font = .systemFont(ofSize: 14, weight: .light)
+        
+        heightTextField.layer.borderWidth = 1
+        heightTextField.borderStyle = .none
+        heightTextField.layer.borderColor = UIColor.gray.cgColor
+        heightTextField.layer.cornerRadius = 20
+
+        weightLabel.text = messages["weight"]
+        weightLabel.textAlignment = .left
+        weightLabel.font = .systemFont(ofSize: 14, weight: .light)
+        
+        weightTextField.layer.borderWidth = 1
+        weightTextField.borderStyle = .none
+        weightTextField.layer.borderColor = UIColor.gray.cgColor
+        weightTextField.layer.cornerRadius = 20
+        
+        randomButton.setTitle(messages["random"], for: .normal)
+        randomButton.setTitleColor(.purple, for: .normal)
+        randomButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        
+        resultButton.setTitle(messages["result"], for: .normal)
+        resultButton.setTitleColor(.white, for: .normal)
+        resultButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        resultButton.layer.cornerRadius = 20
+        resultButton.backgroundColor = .purple
     }
 
-
+        
 }
 
+    
--- README.md
+++ README.md
@@ -20,6 +20,10 @@
 - [ ] 키와 몸무게 입력 받기
     - [ ] 숫자 키패드 사용
     - [ ] Int, Float, Double (정수와 실수)구분
+    - [ ] 몸무게 입력 시 입력값 안보이게 설정
+- [ ] 키보드 내리기
+    - [ ] 백그라운드 tap 했을 때
+    - [ ] 키보드 return 눌렀을 때
 - [ ] BMI 계산하기
     - [ ] BMI 계산 함수 생성
     - [ ] 키와 몸무게 매개변수 받기
@@ -33,12 +37,15 @@
     - [ ] 키/몸무게 범위가 잘못됐을 때 (일반적이지 않은 경우)
     - [ ] 공백이나 빈칸이 입력됐을 때
 ### 디자인
-- [ ] AutoLayout을 활용한 화면 배치 (다양한 기종 대응)
-    - [ ] iPhone SE
-    - [ ] iPhone 13 mini
-    - [ ] iPhone 15
-    - [ ] iPhone 15 Pro
-    - [ ] iphoone 15 Pro Max
+- [x] AutoLayout을 활용한 화면 배치 (다양한 기종 대응)
+    - [x] iPhone SE
+    - [x] iPhone 13 mini
+    - [x] iPhone 15
+    - [x] iPhone 15 Pro
+    - [x] iphoone 15 Pro Max
+- [ ] TextField
+    - [ ] highlight borderColor 변경
+    - [ ] left padding
 - [ ] Alert
 
 <br />
@@ -48,3 +55,16 @@
 | **런치 스크린** | **메인 화면** | **Alert** |
 |:--------:|:-------:|:-----:|
 | | |
+
+<br />
+
+## Question
+<details>
+<summary>Swift에서 상수 선언 시 상수명 명명법</summary>
+<div markdown="1">
+JavaScript에서 상수(Constants) 데이터를 만들 때 객체를 활용해 선언하고, 상수명은 대문자 스네이크 케이스(e.g. TEXT_DATA)를 활용했었다.
+
+Swift에서는 기본으로 카멜 케이스(camelCase)를 사용하고 카멜 케이스 안에서도 Lower Camel Case 와 Upper Camel Case로 나뉘어지는데, 상수 선언의 경우에는 Lower Camel Case를 사용한다고 한다. 따라서 앱에서 사용할 문자열을 딕셔너리(Dictionary)를 활용해 상수를 선언하고, 상수명은 Lower Camel Case 규칙을 적용해주었다.
+
+</div>
+</details>
