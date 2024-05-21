359ffe0b7550695cac4bca10c407e63c1372f190
dev-junehee <dev.junehee@gmail.com>
2024-05-22 01:01:25 +0900

    Feat: 랜덤 BMI 계산 기능 추가

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subTitleLabel: UILabel!
     
@@ -33,6 +32,7 @@ class ViewController: UIViewController {
         "random": "랜덤으로 BMI 계산하기",
         "result": "결과 확인하기",
         "alertTitle": "당신의 BMI 지수 결과입니다",
+        "alertRandomTitle": "랜덤 BMI 지수 결과입니다",
         "alertErrorTitle": "올바른 숫자값을 입력해 주세요!",
         "alertHeightError": "100 ~ 199(cm)",
         "alertWeightError": "0 ~ 200(kg)"
@@ -178,6 +178,27 @@ class ViewController: UIViewController {
         print("몸무게에용", userWeight)
     }
     
+    // 랜덤 계산 버튼 (Int 타입으로만 출력)
+    @IBAction func randomButtonClicked(_ sender: UIButton) {
+        let randomData = [
+            Double.random(in: 100...199),
+            Double.random(in: 0...200)]
+        
+        let randomUserBMI = calculateBMI(height: randomData[0], weight: randomData[1])
+        
+        let alert = UIAlertController(
+            title: messages["alertRandomTitle"],
+            message: "키(cm): \(Int(randomData[0]))\n몸무게(kg): \(Int(randomData[1]))\n BMI:\(Int(randomUserBMI))",
+            preferredStyle: .alert)
+        
+        let open = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(open)
+        present(alert, animated: true)
+        
+        heightTextField.text = ""
+        weightTextField.text = ""
+    }
+    
     // 결과 확인 버튼 핸들
     @IBAction func resultButtonClicked(_ sender: UIButton) {
         // 유저 데이터 중 하나라도 0.0이면 Alert
@@ -214,6 +235,8 @@ class ViewController: UIViewController {
     
     // 키보드 내리기 (tap gesture)
     @IBAction func keyboardDismiss(_ sender: Any) {
+        heightTextField.layer.borderColor = UIColor.gray.cgColor
+        weightTextField.layer.borderColor = UIColor.gray.cgColor
         view.endEditing(true)
     }
         
